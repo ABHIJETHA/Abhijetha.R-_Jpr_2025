6b3b2d62753db94697f045d79950f49c5f2f12a6
ABHIJETHA <144603952+ABHIJETHA@users.noreply.github.com>
2024-01-22 16:44:11 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,31 @@
+#Given an integer N, write a program to count the number of digits in N.
+
+#Input Format
+
+#Example 1: Input0: N = 12345
+
+#Example 2: Input1: N = 8394
+
+#Constraints
+
+#n <= 10000
+
+#Output Format
+
+#Output0: 5 Explanation: N has 5 digits
+
+#Output1: 4 Explanation: N has 4 digits
+
+#Sample Input 0
+
+#12345
+#Sample Output 0
+
+ Read input
+N = int(input())
+
+# Count the number of digits
+num_digits = len(str(N))
+
+# Print the result
+print(num_digits)
